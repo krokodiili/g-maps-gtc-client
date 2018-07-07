2e7828554d804b0007dcb197d18039379c464f8a
Lauri Meltti <laurimeltti@gmail.com>
2018-07-07 14:23:49 +0300

    How to play component

---
+++ src/components/HowToPlay.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+export default () => (
+	<div style={{ padding: 10 }}>
+		<Typography variant="headline"> Pelin idea </Typography>
+		<p style={{ marginTop: 10 }}>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+			aliquip ex ea commodo consequat. Duis aute irure dolor in
+			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+			culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+)
