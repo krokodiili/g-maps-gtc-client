9a7bf753f876a8ea504a51541f8f4898beae1e11
Lauri Meltti <laurimeltti@gmail.com>
2018-07-07 14:24:24 +0300

    layout structure

--- src/App.js
+++ src/App.js
@@ -3,13 +3,48 @@ import './App.css'
 import { Provider } from 'redux-zero/react'
 import store from './store'
 import EnterName from './containers/EnterName'
+import GameLobby from './containers/GameLobby'
+import HowToPlay from './components/HowToPlay'
+import GuessInput from './containers/GuessInput'
+
+const styles = {
+	root: {
+		display: 'flex',
+	},
+	maps: {
+		width: '80vw',
+		height: '90vh',
+		backgroundColor: 'black',
+	},
+	sideBar: {
+		width: '20vw',
+		height: '100vh',
+	},
+	bottomBar: {
+		display: 'flex',
+		justifyContent: 'center',
+		backgroundColor: '#777',
+		height: '100%',
+	},
+}
 
 class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<div className="App">
-					<EnterName />
+				<div className="App" style={styles.root}>
+					<div>
+						<div style={styles.maps} />
+						<div style={styles.bottomBar}>
+							<GuessInput />
+						</div>
+					</div>
+
+					<div style={styles.sideBar}>
+						<EnterName />
+						<GameLobby />
+						<HowToPlay />
+					</div>
 				</div>
 			</Provider>
 		)
