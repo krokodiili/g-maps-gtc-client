93d06d3dbecafd423d1e9d8689330e335791783c
Harri Mähönen <mahonen.h@gmail.com>
2018-07-08 00:04:01 +0300

    added game description

--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -5,13 +5,9 @@ export default () => (
 	<div style={{ padding: 10 }}>
 		<Typography variant="headline"> Pelin idea </Typography>
 		<p style={{ marginTop: 10 }}>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat. Duis aute irure dolor in
-			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-			culpa qui officia deserunt mollit anim id est laborum.
+		Lähdit viime iltana bileettää kavereiden kanssa ja muistikuvat ovat kaikilta kokonaan kadonneet ja
+		tehtävänäsi on ensimmäisenä päätellä kaikki paikat,
+		jossa kävitte. Voittajaksi palkitaan ensimmäinen bailaaja, joka päättelee kaikki viisi kohdetta.
 		</p>
 	</div>
 )
